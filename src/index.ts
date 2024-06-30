09725083d3adb27d01586e3bc09badd0d082f06c
menthy-wu <wumengxuan0728@gmail.com>
2024-06-30 01:04:43 -0700

    install react+tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { window } from "vscode";
+import { commands, ExtensionContext, window } from "vscode";
+import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 
-export function activate() {
+export function activate(context: ExtensionContext) {
   window.showInformationMessage("Hello");
-}
+  const showHelloWorldCommand = commands.registerCommand(
+    "snippet-manager.showHelloWorld",
+    () => {
+      HelloWorldPanel.render(context.extensionUri);
+    },
+  );
 
-export function deactivate() {}
+  // Add command to the extension context
+  context.subscriptions.push(showHelloWorldCommand);
+}
---
+++ webview-ui/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-indigo-600 text-3xl">Hello World</div>
+    </>
+  );
+}
+
+export default App;
